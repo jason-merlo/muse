3ac77bf81cc44239b768e10f44d6207b1611cebf
Steven Ploog <emailstevenp@gmail.com>
2016-04-08 15:34:24 -0500

    Initial commit of Beat_Detection class.
    Will hold beat detection data and functions to clean up the Bar_Matrix a bit.

---
+++ beat_detection.cpp
@@ -0,0 +1,19 @@
+/* ================================================================== *
+*  Written by Steven Ploog
+*
+*  updates:
+*
+*  File: beat_detection.cpp
+*
+*  Description: Class to handle beat detection functions/data
+* ================================================================== */
+
+#include "beat_detection.h"
+
+/* ================================================================== *
+* Beat_Detection
+*
+* ================================================================== */
+Beat_Detection::Beat_Detection() {
+
+}
---
+++ beat_detection.h
@@ -0,0 +1,22 @@
+/* ================================================================== *
+*  Written by Steven Ploog
+*
+*  updates:
+*
+*  File: beat_detection.h
+*
+*  Description: Class declarations for beat detection
+* ================================================================== */
+
+#ifndef BEAT_DETECTION_H
+#define BEAT_DETECTION_H
+
+#include "muse.h"
+
+class Beat_Detection {
+    public:
+        Beat_Detection();
+    private:
+};
+
+#endif // BEAT_DETECTION_H
