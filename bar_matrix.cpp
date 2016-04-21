270fc7d97f1632ce303d9163622f7829873fa543
Steven Ploog <emailstevenp@gmail.com>
2016-04-20 21:45:11 -0500

    Switch to Jason\'s multicolored bars

--- bar_matrix.cpp
+++ bar_matrix.cpp
@@ -140,7 +140,7 @@ void Bar_Matrix::tick(audio_bins * bins, int visualizer_type) {
     switch (visualizer_type) {
         // Visualizers
         case VISUALIZER_BARS:
-        visualizer_bars(bins, 0.15, 0.8, false);
+        visualizer_bars(bins, 0.15, 0.9, false);
         break;
         case VISUALIZER_BARS_MIDDLE:
         visualizer_bars_middle(bins, 0.15, 0.8);
@@ -232,71 +232,91 @@ void Bar_Matrix::pixel_test() {
  * Description: Sets all pixel values to given color value
  * Parameters: [audio_bins]* bins - frequency bins read from chip
  * ================================================================== */
-void Bar_Matrix::visualizer_bars(audio_bins* bins, float in_factor, float out_factor, bool strobe) {
-    decay(out_factor);
-    float bass_level = 0;
+ void Bar_Matrix::visualizer_bars(audio_bins* bins, float in_factor, float out_factor, bool strobe) {
+   decay(out_factor);
+   float bass_level = 0;
+   int red, green, blue;
 
-    if (strobe)
-    bass_level = (log(((bins->left[LEFT_63]+bins->right[LEFT_63])/2.0f)/4096.0f)+0.7f) * 5 * 255.0f;
+   if (strobe)
+     bass_level = (log(((bins->left[LEFT_63]+bins->right[LEFT_63])/2.0f)/4096.0f)+0.7f) * 5 * 255.0f;
 
-    for (char i = 0; i < disp_width; i++) {
-        for (char j = 0; j < disp_height; j++) {
-            // get bin
-            //int level = (i < disp_width/2) ? bar_levels[i] : bar_levels[7-i];
+   for (char i = 0; i < disp_width; i++) {
+     for (char j = 0; j < disp_height; j++) {
+       // get bin
+       //int level = (i < disp_width/2) ? bar_levels[i] : bar_levels[7-i];
 
-            if (strobe)
-            mix_pixel(i, j, 0.5f, bass_level, bass_level, bass_level);
+       if (strobe)
+         mix_pixel(i, j, 0.5f, bass_level, bass_level, bass_level);
 
-            // Set bar levels
-            int level = 0;
-            switch(i) {
-                case 0:
-                level = bins->left[LEFT_63];
-                break;
-                case 1:
-                level = bins->left[LEFT_160];
-                break;
-                case 2:
-                level = bins->left[LEFT_400];
-                break;
-                case 3:
-                level = bins->left[LEFT_1000];
-                break;
-                case 4:
-                level = bins->left[LEFT_2500];
-                break;
-                case 5:
-                level = bins->left[LEFT_6250];
-                break;
-                case 6:
-                level = bins->left[LEFT_16000];
-                break;
-                case 7:
-                level = bins->right[RIGHT_63];
-                break;
-            }
+       // Set bar levels
+       int level = 0;
+       switch(i) {
+         case 0:
+           level = bins->left[LEFT_160];
+           break;
+         case 1:
+           level = bins->left[LEFT_1000];
+           break;
+         case 2:
+           level = bins->left[LEFT_6250];
+           break;
+         case 3:
+           level = bins->left[LEFT_16000];
+           break;
+         case 4:
+           level = bins->right[RIGHT_16000];
+           break;
+         case 5:
+           level = bins->right[RIGHT_6250];
+           break;
+         case 6:
+           level = bins->right[RIGHT_1000];
+           break;
+         case 7:
+           level = bins->right[RIGHT_160];
+           break;
+       }
 
-            level *= FREQ_GAIN;
-            // set bar
-            if (j < (pow((float)(level)/(float)(BINS_MAX), 2)) * (STRIP_LENGTH)) {
-                //if (j < (float)(level)/(float)(BINS_MAX) * (STRIP_LENGTH))
+       level *= FREQ_GAIN;
+       // set bar
+       if (j < (pow((float)(level)/(float)(BINS_MAX), 2)) * (STRIP_LENGTH)) {
+       //if (j < (float)(level)/(float)(BINS_MAX) * (STRIP_LENGTH))
 
-                float val = level*2*PI/4096.0;
-                mix_pixel(i, j, in_factor, bd.r(), bd.g(), bd.b());
-                //mix_pixel(i, j, in_factor, cos(val)*255, cos(val - 2*PI/3)*255, cos(val - 4*PI/3)*255);
-                /*mix_pixel(i, j, in_factor, cos(val)*255, cos(val - 2*pi/3)*255, cos(val - 4*pi/3)*255);*/
+         float val = level*2*PI/4096.0;
 
-                /*mix_pixel(i, j, in_factor, bins->left[0]/16,
-                bins->left[1]/16,
-                bins->left[2]/16);*/
+         // Select colors
+         switch(1) {
+           case 0:
+             red = cos(val)*255;
+             green = cos(val - 2*PI/3)*255;
+             blue = cos(val - 4*PI/3)*255;
+             break;
+           case 1:
+             red = cos(val - 4*PI/3)*255;
+             green = cos(val)*255;
+             blue = cos(val - 2*PI/3)*255;
+             break;
+           case 2:
+             red = cos(val - 2*PI/3)*255;
+             green = cos(val - 4*PI/3)*255;
+             blue = cos(val)*255;
+             break;
+         }
 
-                /*mix_pixel(i, j, in_factor, bins->left[0]/(64-(bins->left[1]/128)),
-                bins->left[1]/(64-(bins->left[2]/128)),
-                bins->left[2]/(64-(bins->left[0]/128)));*/
-            }
-        }
-    }
-}
+         mix_pixel(i, j, in_factor, red, green, blue);
+         /*mix_pixel(i, j, in_factor, cos(val)*255, cos(val - 2*pi/3)*255, cos(val - 4*pi/3)*255);*/
+
+         /*mix_pixel(i, j, in_factor, bins->left[0]/16,
+                                    bins->left[1]/16,
+                                    bins->left[2]/16);*/
+
+         /*mix_pixel(i, j, in_factor, bins->left[0]/(64-(bins->left[1]/128)),
+                                    bins->left[1]/(64-(bins->left[2]/128)),
+                                    bins->left[2]/(64-(bins->left[0]/128)));*/
+       }
+     }
+   }
+ }
 
 /* ================================================================== *
  * Function: visualizer_bars_middle
