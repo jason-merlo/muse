7f78fa4f43db4b0e8dff1cbe15b42c1cc789add1
Steven Ploog <emailstevenp@gmail.com>
2016-05-02 23:06:02 -0500

    Remove all soon to be unused content from webpage.h

--- webpage.h
+++ webpage.h
@@ -11,40 +11,6 @@
 
 #include "application.h"
 
-// Webpage content
-static const char webpage[] =
-"<HTML>"
-"<head>"
-"<style type=\"text/css\">"
-"body {"
-""
-"}"
-"</style>"
-"<head>"
-"<body>"
-"<h1>Spark Webpage!</h1>"
-"</body>"
-"</HTML>";
 
-static const char http_ok[] = "HTTP/1.1 200 OK";
-static const char http_content_type[] = "Content-Type: text/html";
-static const char http_content_length[] = "Content-Length: ";
-static const char http_connection_close[] = "Connection: close";
-
-String webpage_string1=
-"<HTML>\
-<head>\
-<style type=\"text/css\">\
-body {\
-\
-}\
-</style>\
-<head>\
-<body>\
-<h1>Spark Webpage!</h1>\
-</body>\
-</HTML>";
-
-const char index_html[] = "<!DOCTYPE html><html><head><title>Setup your device</title>/head><body><h1>Connect me to your WiFi!</h1></body></html>";
 
 #endif
